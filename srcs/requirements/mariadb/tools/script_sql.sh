21c44ad382a3e438c332628cfc05ce7e013b8634
Lea Devoudelle-Royer <ldevoude@z1r7p5.42lyon.fr>
2026-06-04 11:06:19 +0200

    mariadb + nginx seem to be working together on the VM

--- srcs/requirements/mariadb/tools/script_sql.sh
+++ srcs/requirements/mariadb/tools/script_sql.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-service mariadbd start
+mariadb-install-db --auth-root-authentication-method=normal
+
+until mariadb-admin -u root ping 2>/dev/null; do
+    sleep 1
+done
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -8,3 +13,5 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe;
+
+#ligne 9 faut il espace le -p?
