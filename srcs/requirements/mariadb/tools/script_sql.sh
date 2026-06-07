937bbae1351f55da44918fd642288a3e82347800
Lea Devoudelle-Royer <ldevoude@z1r8p4.42lyon.fr>
2026-06-07 17:42:43 +0200

    thanks a lot tarini... it seem to be working

--- srcs/requirements/mariadb/tools/script_sql.sh
+++ srcs/requirements/mariadb/tools/script_sql.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 mariadb-install-db --auth-root-authentication-method=normal
+fi
 
+mysqld_safe &
 until mariadb-admin -u root ping 2>/dev/null; do
     sleep 1
 done
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'ldevoude.42.fr' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#utiliser IA ici car je voyais vraiment mais alors vraiment pas ou se trouvait mon soucis.
+mysql -e "UPDATE mysql.user SET Password=PASSWORD('${SQL_ROOT_PASSWORD}') WHERE User='root';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe;
-
-#ligne 9 faut il espace le -p?
---
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+until mysqladmin ping -h mariadb -u ${WORDPRESS_DB_USER} -p${WORDPRESS_DB_PASSWORD} --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+cd /var/www/html
+if [ ! -f wp-config.php ]; then
+    wp core download --allow-root
+    wp config create \
+        --dbname=${WORDPRESS_DB_NAME} \
+        --dbuser=${WORDPRESS_DB_USER} \
+        --dbpass=${WORDPRESS_DB_PASSWORD} \
+        --dbhost=mariadb \
+        --allow-root
+    wp core install \
+        --url=${DOMAIN_NAME} \
+        --title=${WP_TITLE} \
+        --admin_user=${WP_ADMIN} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --allow-root
+    wp user create ${WP_USER} ${WP_USER_EMAIL} \
+        --user_pass=${WP_USER_PASSWORD} \
+        --role=author \
+        --allow-root
+
+    
+fi
+exec php-fpm8.2 -F
