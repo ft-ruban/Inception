c6836fb8bc7f638c24786df950603336ca54ac1e
Lea Devoudelle-Royer <ldevoude@z1r8p4.42lyon.fr>
2026-06-09 10:45:55 +0200

    Cleanup(script_sql.sh): cleaned up the script for mariadb

--- srcs/requirements/mariadb/tools/script_sql.sh
+++ srcs/requirements/mariadb/tools/script_sql.sh
@@ -5,13 +5,14 @@ mariadb-install-db --auth-root-authentication-method=normal
 fi
 
 mysqld_safe &
+
 until mariadb-admin -u root ping 2>/dev/null; do
     sleep 1
 done
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'ldevoude.42.fr' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}'";
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 mysql -e "UPDATE mysql.user SET Password=PASSWORD('${SQL_ROOT_PASSWORD}') WHERE User='root';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
